95a43696f414cefd8a7cbe51b351885daceeaba7
Aman Tiwari <amantiwari@gmail.com>
2026-04-15 09:51:32 +0530

    constructor

--- aman.java
+++ aman.java
@@ -1,15 +1,25 @@
 import java.util.Scanner;
 
 public class aman {
-    static void greed() {
-        System.out.println("hello");
-
-
-
+    int a;
+    public aman(int a){
+      this.a=a;
     }
     public static void main(String[] args){
-        greed();
+        aman am=new aman(5);
+        System.out.println(am.a);
+
 
     }
-}
+//    static void greed() {
+//        System.out.println("hello");
+//
+//
+//
+//    }
+//    public static void main(String[] args){
+//        greed();
+
+    }
+
 
