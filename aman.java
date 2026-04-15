ca8e160e2eee7204a6a99b0e461009b6f5e742d3
Aman Tiwari <amantiwari@gmail.com>
2026-04-15 09:44:31 +0530

    without return

--- aman.java
+++ aman.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class aman {
-    static int add(int a, int b) {
-        return a + b;
+    static void greed() {
+        System.out.println("hello");
 
 
 
     }
     public static void main(String[] args){
-        System.out.println(add(7,6));
+        greed();
 
     }
 }
