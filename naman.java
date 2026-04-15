548e1f43891b7b78099c6807c36b27340dd0f53a
Aman Tiwari <amantiwari@gmail.com>
2026-04-15 22:15:08 +0530

    name

--- naman.java
+++ naman.java
@@ -1,9 +1,11 @@
-//import java.util.Scanner;
+import java.util.Scanner;
 public class naman {
     public static void main(String[] args){
-       String a="python";
-        System.out.println(a.length());
-
+//       String a="python";
+//        System.out.println(a.length());
+Scanner sc=new Scanner(System.in);
+String name=sc.nextLine();
+        System.out.println("hello");
     }
 
 }
