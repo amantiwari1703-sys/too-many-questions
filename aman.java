b3718127aa748af1ffca66e644e0c8d0602d6667
Aman Tiwari <amantiwari@gmail.com>
2026-04-15 09:59:57 +0530

    float

--- aman.java
+++ aman.java
@@ -1,10 +1,16 @@
 import java.util.Scanner;
 
 public class aman {
-    int b=7;
-    public static void main(String[] args){
-        aman am=new aman();
-        System.out.println(am.b);
+    public static void main(String[] args) {
+        float e = 23.3f;
+        double r = 24.24;
+        System.out.println(e + r);
+    }
+
+//    int b=7;
+//    public static void main(String[] args){
+//        aman am=new aman();
+//        System.out.println(am.b);
 
     }
-}
+
