bb9d1c373fb82e42fb9e05b250336e8c04713f29
Aman Tiwari <amantiwari@gmail.com>
2026-04-15 14:52:01 +0530

    loop

--- aman.java
+++ aman.java
@@ -2,21 +2,10 @@ import java.util.Scanner;
 
 public class aman {
     public static void main(String[] args) {
-        int day=4;
-        switch(day){
-            case 1:
-                System.out.println("mon");
-                break;
-            case 2:
-                System.out.println("tues");
-                break;
-            case 3:
-                System.out.println("weds");
-                break;
-            default:
-                System.out.println("other");
-
+        for (int a=1;a<=10;a++){
+            System.out.println(a);
         }
 
-    }
-}
+
+          }
+   }
